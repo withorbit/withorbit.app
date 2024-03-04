f3cd3fe116e7cf1baf32a8f257ee5b929b75a255
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-03-03 20:50:25 -0500

    feat: add mode watcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang="ts">
 	import "../app.css";
+	import { ModeWatcher } from "mode-watcher";
 </script>
 
+<ModeWatcher />
 <!-- <ProgressBar /> -->
 
 <div class="flex min-h-screen flex-col overflow-clip">
